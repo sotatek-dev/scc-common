7a5ab730b5851076bd27d16c7c65e9b68a8e1f8c
An Nguyen <nguyenhuuan@gmail.com>
2019-03-03 23:16:04 +0700

    Polish base web server routing

--- src/BaseWebServer.ts
+++ src/BaseWebServer.ts
@@ -41,55 +41,61 @@ export abstract class BaseWebServer {
     });
   }
 
+  protected async createNewAddress(req: any, res: any) {
+    const coin: string = req.params.coin;
+    const address = this.getGateway(coin).createAccount();
+    res.json(address);
+  }
+
+  protected async getAddressBalance(req: any, res: any) {
+    const { coin, address } = req.params;
+    const balance = await this.getGateway(coin).getAddressBalance(address);
+    res.json({ balance });
+  }
+
+  protected async getTransactionDetails(req: any, res: any) {
+    const { coin, txid } = req.params;
+
+    // TODO: Update check txid
+    const tx = await this.getGateway(coin).getOneTransaction(txid);
+    if (!tx) {
+      return res.status(400).json({ error: `Transaction ${txid} is not transfer type.` });
+    }
+
+    const hasMemo = getTokenBySymbol(coin).hasMemo;
+    const entries: any[] = [];
+    const extractedEntries = tx.extractEntries();
+    extractedEntries.forEach(e => {
+      entries.push({
+        address: e.toAddress,
+        value: parseFloat(e.amount),
+        valueString: e.amount,
+      });
+    });
+
+    let resObj = {
+      id: txid,
+      date: '',
+      timestamp: tx.block.timestamp,
+      blockHash: tx.block.hash,
+      blockHeight: tx.block.number,
+      confirmations: tx.confirmations,
+      entries,
+    };
+    if (hasMemo) {
+      resObj = Object.assign({}, resObj, { memo: tx.memo });
+    }
+
+    return res.json(resObj);
+  }
+
   private setup() {
     this.app.use(morgan('dev'));
 
-    this.app.get('/api/:coin/address', async (req, res) => {
-      const coin: string = req.params.coin;
-      const address = this.getGateway(coin).createAccount();
-      res.json(address);
-    });
+    this.app.get('/api/:coin/address', this.createNewAddress.bind(this));
 
-    this.app.get('/api/:coin/address/:address/balance', async (req, res) => {
-      const { coin, address } = req.params;
-      const balance = await this.getGateway(coin).getAddressBalance(address);
-      res.json({ balance });
-    });
+    this.app.get('/api/:coin/address/:address/balance', this.getAddressBalance.bind(this));
 
-    this.app.get('/api/:coin/tx/:txid', async (req, res) => {
-      const { coin, txid } = req.params;
-
-      // TODO: Update check txid
-      const tx = await this.getGateway(coin).getOneTransaction(txid);
-      if (!tx) {
-        return res.status(400).json({ error: `Transaction ${txid} is not transfer type.` });
-      }
-
-      const hasMemo = getTokenBySymbol(coin).hasMemo;
-      const entries: any[] = [];
-      const extractedEntries = tx.extractEntries();
-      extractedEntries.forEach(e => {
-        entries.push({
-          address: e.toAddress,
-          value: parseFloat(e.amount),
-          valueString: e.amount,
-        });
-      });
-      
-      let resObj = {
-        id: txid,
-        date: '',
-        timestamp: tx.block.timestamp,
-        blockHash: tx.block.hash,
-        blockHeight: tx.block.number,
-        confirmations: tx.confirmations,
-        entries,
-      };
-      if (hasMemo) {
-        resObj = Object.assign({}, resObj, { memo: tx.memo });
-      }
-
-      return res.json(resObj);
-    });
+    this.app.get('/api/:coin/tx/:txid', this.getTransactionDetails.bind(this));
   }
 }
