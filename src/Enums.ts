7c615ed6272602bca7197da8eef60928c024c222
mainguyen <mai.nguyen@sotatek.com>
2019-03-19 11:54:42 +0700

    add errors object

--- src/Enums.ts
+++ src/Enums.ts
@@ -47,3 +47,6 @@ Errors.unImplementedError = new ErrorDetails('implement me', 1818);
 Errors.rpcError = new ErrorDetails('rpc node error', 1819);
 Errors.apiEndpointError = new ErrorDetails('api node error', 1819);
 Errors.missPreparedData = new ErrorDetails('some missing on prepared data from database', 1820);
+
+Errors.txInvalid = new ErrorDetails('withdrawal transaction invalid', 2818);
+Errors.txSentFailed = new ErrorDetails('send raw transaction failed', 2819);
