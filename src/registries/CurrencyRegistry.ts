2c1bde6acdb67d4be1a1a8447bcbe7cd4d10cbe0
cong.nguyen <cong.nguyen@sotatek.com>
2019-07-10 17:11:49 +0700

    edit iEosToken config

--- src/registries/CurrencyRegistry.ts
+++ src/registries/CurrencyRegistry.ts
@@ -367,8 +367,8 @@ export class CurrencyRegistry {
       isNative: false,
       isUTXOBased: false,
       code,
-      humanReadableScale: scale,
-      nativeScale: 0,
+      humanReadableScale: 0,
+      nativeScale: scale,
     };
 
     allEosTokens.push(currency);
