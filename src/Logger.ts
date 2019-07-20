eb1bf12d65d492ce20c55a33155aaef67070ecce
An Nguyen <nguyenhuuan@gmail.com>
2019-07-20 20:56:03 +0700

    Polish logger

--- src/Logger.ts
+++ src/Logger.ts
@@ -69,16 +69,16 @@ export function getLogger(name: string, isCloudWatch: boolean = false) {
       return winston.loggers.get(name).debug(msg);
     },
     warn(msg: any) {
-      ERROR_STASHES.push(`[WARN] ${msg}<br><br>`);
       return winston.loggers.get(name).warn(msg);
     },
     error(msg: any) {
+      return winston.loggers.get(name).error(msg);
+    },
+    fatal(msg: any) {
       // Setup error
-      ERROR_STASHES.push(`[ERROR] ${msg}<br><br>`);
+      ERROR_STASHES.push(`[ERROR] ${msg}`);
 
-      // const credentials = s3.config.credentials;
       const env = process.env.NODE_ENV || 'development';
-
       if (env === 'production' || isCloudWatch) {
         const logger = winston.loggers.get(name);
         logger.transports.push(
@@ -95,7 +95,7 @@ export function getLogger(name: string, isCloudWatch: boolean = false) {
   };
 }
 
-async function notifyErrors(message: string[]) {
+async function notifyErrors(messages: string[]) {
   const mailerAccount = EnvConfigRegistry.getCustomEnvConfig('MAILER_ACCOUNT');
   const mailerPassword = EnvConfigRegistry.getCustomEnvConfig('MAILER_PASSWORD');
   const mailerReceiver = EnvConfigRegistry.getCustomEnvConfig('MAILER_RECEIVER');
@@ -119,10 +119,16 @@ async function notifyErrors(message: string[]) {
     from: mailerAccount,
     to: mailerReceiver,
     subject: `[${appName}][${env}] Error Notifier`,
-    html: `${message}`,
+    html: `${messages.join('<br />')}`,
   };
 
-  const info = await transporter.sendMail(mailOptions);
-  console.log('Message sent: %s', info.messageId);
-  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  } catch (err) {
+    console.error(`======= SENDING EMAIL ERROR BEGIN =======`);
+    console.error(err);
+    console.error(`======= SENDING EMAIL ERROR END =======`);
+  }
 }
