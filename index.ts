bd649b230a4e5abc35031f9620596d9dedc43468
An Nguyen <nguyenhuuan@gmail.com>
2019-11-02 18:52:53 +0700

    Add place holder for environment variables decryption

--- index.ts
+++ index.ts
@@ -11,6 +11,9 @@ if (process.env.isEnvSet_KnV5Ha0UlAAEME69I6KA === '1') {
 }
 process.env.isEnvSet_KnV5Ha0UlAAEME69I6KA = '1';
 
+// Try to decrypt env variables if they're encrypted
+import './EnvDecrypt';
+
 import * as Utils from './src/Utils';
 export { Utils };
 
