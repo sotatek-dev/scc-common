411479e19e5b06d412b539f5651e65e8d3bfa133
Sinh Vu <sinh.vu@sotatek.com>
2019-11-21 16:12:09 +0700

    refactor mail service

--- src/Logger.ts
+++ src/Logger.ts
@@ -15,6 +15,8 @@ if (!ERROR_SENDING_INTERVAL || isNaN(ERROR_SENDING_INTERVAL)) {
   ERROR_SENDING_INTERVAL = 15 * 60 * 1000;
 }
 
+let _mailCallback: (props: any) => Promise<void>;
+
 setInterval(notifyErrors, ERROR_SENDING_INTERVAL);
 
 const enumerateErrorFormat = winston.format(info => {
@@ -88,6 +90,11 @@ export function getLogger(name: string) {
   };
 }
 
+export function registerMailEventCallback(callback: (props: any) => Promise<void>) {
+  _mailCallback = callback;
+  getLogger('Logger').info(`MailService::A callback has just been registered`);
+}
+
 async function notifyErrors() {
   if (!ERROR_STASHES.length) {
     return;
@@ -103,6 +110,22 @@ async function notifyErrors() {
 
   const appName: string = process.env.APP_NAME || 'Exchange Wallet';
   const env: string = process.env.NODE_ENV || 'development';
+  const sender = EnvConfigRegistry.getCustomEnvConfig('MAIL_FROM_ADDRESS');
+  const senderName = `${EnvConfigRegistry.getCustomEnvConfig('MAIL_FROM_NAME')} <${sender}>`;
   const subject = `[${appName}][${env}] Error Notifier`;
+  if (_mailCallback) {
+    await _mailCallback({
+      senderAddress: sender,
+      senderName,
+      recipientAddress: mailReceiver,
+      title: subject,
+      templateName: 'logger_error_notifier_layout.hbs',
+      content: {
+        error_message: messages.join('<br />'),
+      },
+    });
+    return;
+  }
+
   Utils.sendMail(mailReceiver, subject, `${messages.join('<br />')}`);
 }
