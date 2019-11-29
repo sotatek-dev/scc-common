f63b2d99012786c904aaae0f24f01e6b80c1fcf2
quan.ta <quan.ta@sotatek.com>
2019-11-29 17:45:56 +0700

    add MultiEntriesTransaction

---
+++ src/types/MultiEntriesTransaction.ts
@@ -0,0 +1,78 @@
+import { Transaction } from './Transaction';
+import { ICurrency } from '../interfaces';
+import { BlockHeader } from './BlockHeader';
+import TransferEntry from './TransferEntry';
+import { BigNumber } from 'bignumber.js';
+
+export interface IMultiEntriesTxEntry {
+  readonly address: string;
+  readonly currency: ICurrency;
+  readonly amount: string;
+}
+
+export interface IMultiEntriesTxProps {
+  readonly txid: string;
+  readonly inputs: IMultiEntriesTxEntry[];
+  readonly outputs: IMultiEntriesTxEntry[];
+  readonly block: BlockHeader;
+  readonly lastNetworkBlockNumber: number;
+}
+
+export abstract class MultiEntriesTransaction extends Transaction {
+  protected readonly outputs: IMultiEntriesTxEntry[];
+  protected readonly inputs: IMultiEntriesTxEntry[];
+
+  constructor(props: IMultiEntriesTxProps) {
+    super(
+      {
+        confirmations: props.lastNetworkBlockNumber - props.block.number + 1,
+        height: props.block.number,
+        timestamp: props.block.timestamp,
+        txid: props.txid,
+      },
+      props.block
+    );
+    this.inputs = props.inputs;
+    this.outputs = props.outputs;
+  }
+
+  public _extractEntries(): TransferEntry[] {
+    const entries: TransferEntry[] = [];
+
+    this.inputs.forEach(vIn => {
+      const entry = this._convertVInToTransferEntry(vIn);
+      if (entry) {
+        entries.push(entry);
+      }
+    });
+
+    this.outputs.forEach(vOut => {
+      const entry = this._convertVoutToTransferEntry(vOut);
+      if (entry) {
+        entries.push(entry);
+      }
+    });
+
+    return TransferEntry.mergeEntries(entries);
+  }
+
+  protected _convertVInToTransferEntry(vIn: IMultiEntriesTxEntry): TransferEntry {
+    return {
+      amount: new BigNumber(-vIn.amount),
+      currency: vIn.currency,
+      address: vIn.address,
+      txid: this.txid,
+      tx: this,
+    };
+  }
+
+  protected _convertVoutToTransferEntry(vOut: IMultiEntriesTxEntry): TransferEntry {
+    return {
+      amount: new BigNumber(vOut.amount),
+      currency: vOut.currency,
+      address: vOut.address,
+      txid: this.txid,
+      tx: this,
+    };
+  }
+}
