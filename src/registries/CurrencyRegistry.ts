c339e2be719811304bdd1a516130451fae8145a6
truong-vcc <truong.nguyen@vcc.exchange>
2021-03-22 16:25:15 +0700

    fix package.json

--- src/registries/CurrencyRegistry.ts
+++ src/registries/CurrencyRegistry.ts
@@ -242,6 +242,18 @@ const Binance = {
   hasMemo: true,
 };
 
+const BinanceCoin = {
+  symbol: BlockchainPlatform.BinanceSmartChain,
+  networkSymbol: BlockchainPlatform.BinanceSmartChain,
+  name: 'BinanceCoin',
+  platform: BlockchainPlatform.BinanceSmartChain,
+  isNative: true,
+  isUTXOBased: false,
+  humanReadableScale: 8,
+  nativeScale: 0,
+  hasMemo: false,
+};
+
 const Terra = {
   symbol: BlockchainPlatform.Terra,
   networkSymbol: BlockchainPlatform.Terra,
@@ -300,6 +312,7 @@ const nativeCurrencies: ICurrency[] = [
   Nem,
   Tron,
   Binance,
+  BinanceCoin,
   Terra,
   Cosmos,
   BitcoinValue,
@@ -323,6 +336,7 @@ export class CurrencyRegistry {
   public static readonly Nem: ICurrency = Nem;
   public static readonly Tron: ICurrency = Tron;
   public static readonly Binance: ICurrency = Binance;
+  public static readonly BinanceCoin: ICurrency = BinanceCoin;
   public static readonly Terra: ICurrency = Terra;
   public static readonly Cosmos: ICurrency = Cosmos;
   public static readonly BitcoinValue: ICurrency = BitcoinValue;
